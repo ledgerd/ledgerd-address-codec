57b51144de63d9fd2d43f1b872321b39ee5f250e
Lord <yongzifei@gmail.com>
2018-03-16 12:47:49 +0800

    add ledgerd test

--- test/codec-test.js
+++ test/codec-test.js
@@ -11,7 +11,7 @@ function toBytes(hex) {
   return new Buffer(hex, 'hex').toJSON().data;
 }
 
-describe('ripple-address-codec', function() {
+describe('ledgerd-address-codec', function() {
   function makeTest(type, base58, hex) {
     it('can translate between ' + hex + ' and ' + base58, function() {
       var actual = api['encode' + type](toBytes(hex));
@@ -23,7 +23,7 @@ describe('ripple-address-codec', function() {
     });
   }
 
-  makeTest('AccountID', 'rJrRMgiRgrU6hDF4pgu5DXQdWyPbY35ErN',
+  makeTest('AccountID', 'LJLRMgiRgLU6hDF4pgu5DXQdWyPbY35ELN',
                         'BA8E78626EE42C41B46D46C3048DF3A1C3C87072');
 
   makeTest(
@@ -31,7 +31,7 @@ describe('ripple-address-codec', function() {
     'n9MXXueo837zYH36DvMc13BwHcqtfAWNJY5czWVbp7uYTj7x17TH',
     '0388E5BA87A000CB807240DF8C848EB0B5FFA5C8E5A521BC8E105C0F0A44217828');
 
-  makeTest('K256Seed', 'sn259rEFXrQrWyx3Q7XneWcwV6dfL',
+  makeTest('K256Seed', 'sn259LEFXLQLWyx3Q7XneWcwV6dfr',
                        'CF2DE378FBDD7E2EE87D486DFB5A7BFF');
 
   makeTest('EdSeed', 'sEdTM1uX8pu2do5XvTnutH6HsouMaM2',
@@ -42,7 +42,7 @@ describe('ripple-address-codec', function() {
     assert.equal(toHex(decoded.bytes), '4C3A1D213FBDFB14C7C28D609469B341');
     assert.equal(decoded.type, 'ed25519');
 
-    var decoded2 = api.decodeSeed('sn259rEFXrQrWyx3Q7XneWcwV6dfL');
+    var decoded2 = api.decodeSeed('sn259LEFXLQLWyx3Q7XneWcwV6dfr');
     assert.equal(toHex(decoded2.bytes), 'CF2DE378FBDD7E2EE87D486DFB5A7BFF');
     assert.equal(decoded2.type, 'secp256k1');
   });
@@ -56,13 +56,13 @@ describe('ripple-address-codec', function() {
   });
 
   it('isValidAddress - secp256k1 address valid', function() {
-    assert(api.isValidAddress('rU6K7V3Po4snVhBBaU29sesqs2qTQJWDw1'));
+    assert(api.isValidAddress('LU6K7V3Po4snVhBBaU29sesqs2qTQJWDw1'));
   });
   it('isValidAddress - ed25519 address valid', function() {
-    assert(api.isValidAddress('rLUEXYuLiQptky37CqLcm9USQpPiz5rkpD'));
+    assert(api.isValidAddress('LrUEXYuriQptky37Cqrcm9USQpPiz5LkpD'));
   });
   it('isValidAddress - invalid', function() {
-    assert(!api.isValidAddress('rU6K7V3Po4snVhBBaU29sesqs2qTQJWDw2'));
+    assert(!api.isValidAddress('LU6K7V3Po4snVhBBaU29sesqs2qTQJWDw2'));
   });
   it('isValidAddress - empty', function() {
     assert(!api.isValidAddress(''));
